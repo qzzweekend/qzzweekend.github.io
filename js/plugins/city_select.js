40f80c4d672d4fc9e3feb884bb406d2ff2a691f7
zhenye19899 <qizz89@163.com>
2016-11-12 01:44:14 +0800

    城市选择组件完善

--- js/plugins/city_select.js
+++ js/plugins/city_select.js
@@ -1,77 +1,88 @@
 /**
- *@城市列表选择组件
- * Created by qzz on 2016/11/11.
+ * @ 城市列表选择组件  citySelect
+ * @ author qizhenzhen
+ * @ Created by qzz on 2016/11/11.
  */
-
-function CityList(){
-    if(!arguments.length){
+var provinceItem, cityItem;
+function CityList() {
+    if (!arguments.length) {
         return;
     }
 
-    if(jQuery){
-        this.init.apply(this,arguments);
-    }else{
+    if (jQuery) {
+        this.init.apply(this, arguments);
+    } else {
         alert('需要jQuery');
     }
 }
 
-CityList.prototype={
-    constructor:CityList,
-    init:function(options){
-        this.id=options.id;
+CityList.prototype = {
+    constructor: CityList,
+    init: function (options) {
+        this.id = options.id;
         this.createPanel();
         this.inputEvent(options.id);
     },
 
-    createPanel:function(){
-        var cityContent=[
-            '<div class="week_mask"></div>'+
-            '<div class="city_select_pop">'+
-            '<div class="city_pop_head">'+
-            '<span class="city_cancel">取消</span>'+
-            '<h3>选择城市</h3>'+
-            '</div>'+
-            '<div class="city_pop_content">'+
-            '<div class="city_left_wrap">'+
-            '<ul class="city_left_ul">'+
-            '<li class="city_left_item active">上海</li>'+
-            '<li class="city_left_item">北京</li>'+
-            '<li class="city_left_item">天津</li>'+
-            '<li class="city_left_item">河北</li>'+
-            '<li class="city_left_item">山西</li>'+
-            '<li class="city_left_item">内蒙古</li>'+
-            '<li class="city_left_item">四川</li>'+
-            '<li class="city_left_item">海南</li>'+
-            '</ul>'+
-            '</div>'+
-            '<div class="city_pop_right_wrap">'+
-            '<ul class="city_pop_right_ul">'+
-            '<li class="city_right_item">全上海</li>'+
-            '<li class="city_right_item">黄浦区</li>'+
-            '<li class="city_right_item">卢湾区</li>'+
-            '<li class="city_right_item">徐汇区</li>'+
-            '<li class="city_right_item">长宁区</li>'+
-            '<li class="city_right_item">静安区</li>'+
-            '<li class="city_right_item">黄浦区</li>'+
-            '<li class="city_right_item">卢湾区</li>'+
-            '<li class="city_right_item">徐汇区</li>'+
-            '<li class="city_right_item">长宁区</li>'+
-            '<li class="city_right_item">静安区</li>'+
-            '</ul>'+
-            '</div>'+
-            '</div>'+
+    createPanel: function () {
+        var provinceContent = [
+            '<div class="week_mask"></div>' +
+            '<div class="city_select_pop">' +
+            '<div class="city_pop_head">' +
+            '<span class="city_cancel">取消</span>' +
+            '<h3>选择城市</h3>' +
+            '</div>' +
+            '<div class="city_pop_content">' +
+            '<div class="city_left_wrap">' +
+            '<ul class="city_left_ul">' +
+            '<%for(var i=0;i<cityData_province.length; i++){ %>' +
+            '<li class="city_left_item active"><%=cityData_province[i]%></li>' +
+            '<% } %>' +
+            '</ul>' +
+            '</div>' +
+            '<div class="city_pop_right_wrap">' +
+                //右边城市
+            '</div>' +
+            '</div>' +
             '</div>'
         ].join('');
 
-        var city_wrap=$('<div></div>');
-        city_wrap.html(cityContent).appendTo($(document.body));
+
+        var city_wrap = $('<div></div>');
+
+        //省模板替换
+        cityData_province = JSON.parse(localStorage.cityData_province);
+        console.log(cityData_province);
+        provinceItem = ejs.render(provinceContent, {cityData_province: cityData_province})
+        $(provinceItem).appendTo($(document.body));
+
+        //初始化市模板
+        this.cityShow();
 
     },
 
-    inputEvent:function(id){
-        var that=this;
+    cityShow: function (index) {
+        index = index || 0;
+        var cityContent = [
+            '<ul class="city_pop_right_ul">' +
+            '<% for(var i=0;i<cityData_city.length; i++){ %>' +
+            '<li class="city_right_item"><%=cityData_city[i]%></li>' +
+            '<% } %>' +
+            '</ul>'
+        ].join('');
+
+        //市模板替换
+        cityData_city = JSON.parse(localStorage.cityData_city);
+        console.log(cityData_city[0]);
+        cityItem = ejs.render(cityContent, {cityData_city: cityData_city[index]});
+        $('.city_pop_right_wrap').html('');
+        $(cityItem).appendTo($('.city_pop_right_wrap'));
+    },
+
+    inputEvent: function (id) {
+        var that = this;
         $(id).on('click', function () {
-            var that=$(this).find('#city_content');
+            var _this = $(this).find('#city_content');
             $('.week_mask').fadeIn(100);
             $('.city_select_pop').animate({
                 bottom: 0
@@ -85,7 +96,7 @@ CityList.prototype={
                 }, 200);
             });
 
-            $('.week_mask').click(function(){
+            $('.week_mask').click(function () {
                 $(this).fadeOut(100);
                 $('.city_select_pop').animate({
                     bottom: '-10rem'
@@ -95,12 +106,13 @@ CityList.prototype={
             //城市区点击时
             $('.city_pop_content').click(function (e) {
                 var target = $(e.target);
-                if (target.hasClass('city_left_item') ) {
+                if (target.hasClass('city_left_item')) {
                     $('.city_left_ul >li').removeClass('active');
                     target.addClass('active');
-                }else if(target.hasClass('city_right_item')){
-                    var cityName=$('.city_left_ul >li.active').html()+'&nbsp;&nbsp;'+target.html();
-                    that.html('<i class="city_text">'+cityName+'</i>');
+                    that.cityShow(target.index());
+                } else if (target.hasClass('city_right_item')) {
+                    var cityName = $('.city_left_ul >li.active').html() + '&nbsp;&nbsp;' + target.html();
+                    _this.html('<i class="city_text">' + cityName + '</i>');
                     $('.week_mask').fadeOut(100);
                     $('.city_select_pop').animate({
                         bottom: '-10rem'
